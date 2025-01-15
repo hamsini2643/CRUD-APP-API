894b17f486da3ef9ee31a1d68e9ee228ed5a36e6
Hamsini <hamsini.r@tenxerlabs.com>
2025-01-15 16:08:07 +0530

    ci: :white_check_mark: added env

--- database.py
+++ database.py
@@ -1,12 +1,12 @@
 from sqlalchemy.orm import declarative_base, sessionmaker
 from sqlalchemy import create_engine
-#import os
-#from dotenv import load_dotenv
+import os
+from dotenv import load_dotenv
 
 
-#load_dotenv()
-#engine=create_engine(os.get_env(DATABASE_CONNECTION_STRING),echo=True)
-engine=create_engine("postgresql://postgres:Jasmine@localhost/Person",echo=True)
+load_dotenv()
+engine=create_engine(os.get_env(DATABASE_CONNECTION_STRING),echo=True)
+#engine=create_engine("postgresql://postgres:Jasmine@localhost/Person",echo=True)
 
 Base=declarative_base()
 SessionLocal=sessionmaker(engine)
