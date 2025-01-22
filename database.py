2d166de961fe0b226152d7d8d855883d0dfbab06
Hamsini <hamsini.r@tenxerlabs.com>
2025-01-22 15:44:36 +0530

    perf: :art: Routing,docker,gitignore

--- database.py
+++ database.py
@@ -7,7 +7,12 @@ from sqlalchemy.ext.declarative import declarative_base
 
 #load_dotenv()
 #engine=create_engine(os.getenv(DATABASE_CONNECTION_STRING),echo=True)
-engine=create_engine("postgresql://postgres:Jasmine@localhost/Person",echo=True)
+host = "127.0.0.1"
+user = "postgres"
+password = "Jasmine"
+db_name = "Person"
+DATABASE_CONNECTION_STRING=f"postgresql://{user}:{password}@{host}/{db_name}"
+engine=create_engine(DATABASE_CONNECTION_STRING, echo=True)
 
 Base=declarative_base()
 SessionLocal=sessionmaker(engine)
--- migrations/env.py
+++ migrations/env.py
@@ -6,7 +6,7 @@ from sqlmodel import SQLModel
 from pathlib import Path 
 from alembic import context
 
-from models import Person 
+from app.models import Person 
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
