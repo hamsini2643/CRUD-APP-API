9623356783c3179f0ca5dc085fe2836d0961127d
Hamsini <hamsini.r@tenxerlabs.com>
2025-01-23 11:19:07 +0530

    feat: :sparkles: added migration for reservation table
    added foreign key to Person table

--- app/models.py
+++ app/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import String, Integer, Column, Boolean
+from sqlalchemy import String, Integer, Column, Boolean, ForeignKey, TIMESTAMP
 
 from database import Base,engine
 def create_tables():
@@ -9,5 +9,9 @@ class Person(Base):
     firstname=Column(String(40),nullable=False)
     lastname=Column(String(40),nullable=False)
     is_male=Column(Boolean)
-
-    
+class Slots(Base):
+    __tablename__='slots'
+    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    start_time = Column(TIMESTAMP, nullable=False)
+    end_time = Column(TIMESTAMP, nullable=False)
+    person_id = Column(Integer, ForeignKey("person.id", on_delete="CASCADE"), nullable=False)  # Foreign key reference to 'person' table    
